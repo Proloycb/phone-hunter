873c7ac0391b02b16221d5041ec76fab20637c1f
Proloycb <proloychacroborty@gmail.com>
2022-03-01 13:13:53 +0600

    responsive finished

--- js/app.js
+++ js/app.js
@@ -1,8 +1,24 @@
+// toggle spinner
+const toggleSpinner = displayStyle => {
+    document.getElementById('spinner').style.display = displayStyle;
+}
+toggleSpinner('none');
+// toggle search detail
+const toggleSearchDetail = displayStyle => {
+    document.getElementById('search-detail').style.display = displayStyle;
+}
+// show all 
+// const showAllButton = displayStyle => {
+//     document.getElementById('show-all').style.display = displayStyle;
+// }
+// showAllButton('none');
 const loadData = () => {
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value ;
     // clear the value
     searchField.value = '';
+    toggleSpinner('block');
+    toggleSearchDetail('none');
     if(searchText == ''){
         return alert('please add something');
     }
@@ -11,16 +27,18 @@ const loadData = () => {
     fetch(url)
     .then(res => res.json())
     .then(phone => displayResult(phone.data.slice(0, 20)));
-    // document.getElementById('show-more').addEventListener(click, function(){
-    //     displayResult(phone.data.slice(21));
-    // })
+    // document.getElementById('show-more').innerHTML = `
+    //     <button onclick = "${displayResult(phone.data)}" class="btn btn-info text-white">Show All</button> 
+    // `;
+    
+
+   
     }
 }
 
 // error message show
 
- const errorMessage = document.getElementById('error-show').style.display = 'none';
-
+const errorMessage = document.getElementById('error-show').style.display = 'none';
 
 const displayResult = phones => {
     const searchResult = document.getElementById('search-result');
@@ -44,11 +62,11 @@ const displayResult = phones => {
         `;
         searchResult.appendChild(div);
     })
-    console.log(phones);
+    toggleSpinner('none');
+    showAllButton('block');
 }
-
-const loadPhoneDetails = slug => {
-    const url = `https://openapi.programming-hero.com/api/phone/${slug}`;
+const loadPhoneDetails = phoneId => {
+    const url = `https://openapi.programming-hero.com/api/phone/${phoneId}`;
     fetch(url)
     .then(res => res.json())
     .then(phone => displayDetailResult(phone.data));
@@ -57,6 +75,7 @@ const loadPhoneDetails = slug => {
 const displayDetailResult = phone => {
     const searchDetails = document.getElementById('search-detail');
     searchDetails.textContent='';
+    toggleSearchDetail('block');
     const div = document.createElement('div');
     div.classList.add('card');
     div.innerHTML = `
@@ -64,8 +83,8 @@ const displayDetailResult = phone => {
         <div class="card-body">
             <h5 class="card-title">${phone.name}</h5>
             <p class="card-text">${phone.brand}</p>
-            <p class="card-text"><strong>ReleaseDate: </strong>${phone?.releaseDate||'releasedate upcoming'}</p>
-            <p class="card-text"><strong>storage: </strong>${phone.mainFeatures.storage}, <strong>displaySize: </strong>${phone.mainFeatures.displaySize},<strong> chipSet: </strong>${phone.mainFeatures.chipSet},<strong> memory: </strong>${phone.mainFeatures.memory}</p>
+            <p class="card-text"><strong>ReleaseDate: </strong>${phone?.releaseDate||'Releasedate upcoming'}</p>
+            <p class="card-text"><strong>Storage: </strong>${phone.mainFeatures.storage}, <strong>DisplaySize: </strong>${phone.mainFeatures.displaySize},<strong> ChipSet: </strong>${phone.mainFeatures.chipSet},<strong> Memory: </strong>${phone.mainFeatures.memory}</p>
             <p class="card-text"><strong>Sensors: </strong>${phone.mainFeatures.sensors}</p>
             <p class="card-text"><strong>WLAN: </strong>${phone?.others?.WLAN|| 'no data found!'}, <strong>Bluetooth: </strong>${phone?.others?.Bluetooth|| 'no data found!'},<strong>GPS: </strong> ${phone?.others?.GPS|| 'no data found!'},<strong> NFC: </strong>${phone?.others?.NFC|| 'no data found!'},<strong> Radio: </strong>${phone?.others?.Radio|| 'no data found!'},<strong> USB: </strong>${phone?.others?.USB|| 'no data found!'}</p>
         </div>
